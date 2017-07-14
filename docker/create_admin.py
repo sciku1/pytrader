566e000d0fb1d91469af5eacd3cfdeda0fb44ed3
Timothy Lopez <timothy.lopez38@gmail.com>
2017-07-13 18:16:53 -0600

    Changed the way settings are set

--- docker/create_admin.py
+++ docker/create_admin.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from django.contrib.auth.models import User
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pypolo.settings")
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pypolo.settings'
 username = os.environ.get("PYTRADER_USER", 'trader')
 password = os.environ.get("PYTRADER_PASSWORD", 'trader')
 if not User.objects.filter(username=username).exists():
